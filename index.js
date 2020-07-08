9bf93ef1c43cfe83862a2581ce694c5c3b67bc3d
Mariya <mariya.podosinova@gmail.com>
2020-07-08 11:04:02 +0100

    Adding bg images for drums, Switch for every btn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (let i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function () {
-    alert("I got clicked");
+    let audio = new Audio("./sounds/tom-1.mp3");
+    audio.play();
   });
 }
